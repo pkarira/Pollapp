1504c5a7615c052a5e3f23014a81f98a038e6f81
pkarira <pulkit5208@gmail.com>
2017-08-11 03:11:37 +0530

    made results api

--- polls/views.py
+++ polls/views.py
@@ -1,30 +1,64 @@
 from time import timezone
-
+import json
 from django.http import HttpResponse
 
 from polls.models import Question, Choice
 
 
 def index(request):
-    s = ""
+    outerJson = {}
+    results = []
+    choices = []
+    singleChoice = {}
+    singleQuestion = {}
     for question in Question.objects.all():
-        s += question.question_text + " "
+        singleQuestion["questions"] = question.question_text
+        singleQuestion["id"] = question.id
         for choice in question.choice_set.all():
-            s += choice.choice_text + " "
-        s += "\n"
-    return HttpResponse(s)
+            singleChoice["text"] = choice.choice_text
+            singleChoice["votes"] = choice.votes
+            singleChoice["id"] = choice.id
+            choices.append(singleChoice)
+            singleChoice = {}
+        singleQuestion["choices"] = choices
+        results.append(singleQuestion)
+        choices = []
+        singleQuestion = {}
+    outerJson["results"] = results
+    return HttpResponse(json.dumps(outerJson), content_type="application/json")
 
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+def totalResults(request):
+    outerJson = {}
+    results = []
+    choices = []
+    singleChoice = {}
+    singleQuestion = {}
+    for question in Question.objects.all():
+        singleQuestion["questions"] = question.question_text
+        singleQuestion["id"] = question.id
+        for choice in question.choice_set.all():
+            singleChoice["text"] = choice.choice_text
+            singleChoice["votes"] = choice.votes
+            singleChoice["id"] = choice.id
+            choices.append(singleChoice)
+            singleChoice = {}
+        singleQuestion["choices"] = choices
+        results.append(singleQuestion)
+        choices = []
+        singleQuestion = {}
+    outerJson["results"] = results
+    return HttpResponse(json.dumps(outerJson), content_type="application/json")
 
 
-def vote(request, question_id):
+def vote(request, question_id, choice_id):
+    selected_choice = Question.objects.get(pk=question_id).choice_set(pk=choice_id)
+    selected_choice.votes += 1
+    selected_choice.save()
     return HttpResponse("You're voting on question %s." % question_id)
 
 
@@ -36,10 +70,45 @@ def addQuestion(question, choice1, choice2):
 
 
 def getQuestions(request):
+    outerJson = {}
+    dataJson = {}
+    questionArray = []
+    choices = []
+    singleChoice = {}
+    singleQuestion = {}
+    for question in Question.objects.all():
+        singleQuestion["questions"] = question.question_text
+        singleQuestion["id"]=question.id
+        for choice in question.choice_set.all():
+            singleChoice["text"] = choice.choice_text
+            choices.append(singleChoice)
+            singleChoice = {}
+        singleQuestion["choices"] = choices
+        questionArray.append(singleQuestion)
+        choices = []
+        singleQuestion = {}
+    dataJson["question"] = questionArray
+    outerJson["data"] = dataJson
+    return HttpResponse(json.dumps(outerJson), content_type="application/json")
+
+
+def changeChoiceOption(request, choice_id, text):
+    choice = Choice.objects.get(pk=choice_id)
+    choice.choice_text = text
+    choice.save()
+
+
+def totalResults(request):
     s = ""
     for question in Question.objects.all():
         s += question.question_text + " "
-        for choice in question.choice_set.all:
-            s += choice.choice_text + " "
+        for choice in question.choice_set.all():
+            s += choice.choice_text + " " + str(choice.votes)
         s += "\n"
     return HttpResponse(s)
+
+
+def changeQuestion(request, question_id, text):
+    question = Question.objects.get(pk=question_id)
+    question.question_text = text
+    question.save()
