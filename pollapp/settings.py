a14c42c3f8c68c3ca7f995975e22bda19bd77b9c
pkarira <pulkit5208@gmail.com>
2017-09-25 19:15:51 +0530

    removed statis files

--- pollapp/settings.py
+++ pollapp/settings.py
@@ -27,11 +27,6 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '*'
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 # Application definition
 
 INSTALLED_APPS = [
